ca4196f17301cc906027ae5ac57f6266545e957c
acevinh <150435649+acevinh@users.noreply.github.com>
2024-01-14 17:13:48 +0700

    full thuc hanh 2

--- Thuchanh2-arrays-and-objects/03-array-nesting-concat-spread/script.js
+++ Thuchanh2-arrays-and-objects/03-array-nesting-concat-spread/script.js
@@ -7,38 +7,42 @@ const berries = ['strawberry', 'blueberry', 'rasberry'];
 
 // Nesting berries inside of fruits
 fruits.push(berries);
+console.log(fruits);
 
 // Now we can access 'blueberry' with the following
 x = fruits[3][1];
+// console.log(x);
 
-// Create a new variable and nest both arrays
+// // Create a new variable and nest both arrays
 const allFruits = [fruits, berries];
+console.log(allFruits);
 
-x = allFruits[1][2];
+// x = allFruits[1][2];
 
-// concat() - Concatenate arrays
+// // concat() - Concatenate arrays
 x = fruits.concat(berries);
+// console.log(x);
 
-// spread operator (...) - Concatenate with
-x = [...fruits, ...berries];
+// // spread operator (...) - Concatenate with
+// x = [...fruits, ...berries];
 
-// flat() - Flatten an array
-const arr = [1, 2, [3, 4, 5], 6, [7, 8]];
-x = arr.flat();
+// // flat() - Flatten an array
+// const arr = [1, 2, [3, 4, 5], 6, [7, 8]];
+// x = arr.flat();
 
-// Static methods of Array object
+// // Static methods of Array object
 
-// isArray() - Check is is an array
-x = Array.isArray(fruits);
+// // isArray() - Check is is an array
+// x = Array.isArray(fruits);
 
-// from() - Create an array from an array like value
-x = Array.from('12345');
+// // from() - Create an array from an array like value
+// x = Array.from('12345');
 
-// of() - Create an array from a set of values
-const a = 1;
-const b = 2;
-const c = 3;
+// // of() - Create an array from a set of values
+// const a = 1;
+// const b = 2;
+// const c = 3;
 
-x = Array.of(a, b, c);
+// x = Array.of(a, b, c);
 
-console.log(x);
+// console.log(x);
--- Thuchanh2-arrays-and-objects/04-array-challenge/script.js
+++ Thuchanh2-arrays-and-objects/04-array-challenge/script.js
@@ -7,9 +7,9 @@ const arr = [1, 2, 3, 4, 5];
 // arr.unshift(6);
 
 // Same result as above
-arr.push(6);
-arr.unshift(0);
-arr.reverse();
+arr.push(6);// thêm vào cuối
+arr.unshift(7);// thêm vào đầu
+// arr.reverse(); // đảo chiều
 
 console.log(arr);
 
@@ -19,10 +19,12 @@ const arr1 = [1, 2, 3, 4, 5];
 const arr2 = [5, 6, 7, 8, 9, 10];
 
 // Solution 1
-const arr3 = arr1.slice(0, 4).concat(arr2);
+
+// console.log(arr1.slice(0, 4));// cắt
+const arr3 = arr1.slice(0, 4).concat(arr2);// nối 
 
 // Solution 2
-const arr4 = [...arr1, ...arr2];
-arr4.splice(4, 1);
+const arr4 = [...arr1, ...arr2];// nối cách 2
+// arr4.splice(4, 1);
 
 console.log(arr4);
